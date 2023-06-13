683185506b8efda82fcb9c6ab7c697be6aee8e0c
Eduardo <eduangelinicarvalho@gmail.com>
2023-06-13 20:40:58 -0300

    commit#3-styles-and-footer

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,21 +1,20 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
-import banner from './banner.png';
+import Banner from "../../componentes/Banner";
 import styles from './PaginaInicial.module.scss';
+import Rodape from "../../componentes/Rodape";
 
 export default function PaginaInicial() {
   return (
     <>
       <Cabecalho />
       <main>
-        <section className={styles.principal}>
+        <div className={styles.principal}>
           <Menu />
-          <div className={styles.principal__imagem}>
-            <h1>A galeria mais completa do espaço</h1>
-            <img src={banner} alt="A imagem da terra vista do espaço" />
-          </div>
-        </section>
+          <Banner />
+        </div>
       </main>
+      <Rodape />
     </>
   )
 }
