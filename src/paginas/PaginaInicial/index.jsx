adf0ae21e571795b1aa9f9bee5f8981249ad12f2
Eduardo <eduangelinicarvalho@gmail.com>
2023-06-14 00:36:43 -0300

    commit#4-Final-project

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,17 +1,24 @@
-import Cabecalho from "../../componentes/Cabecalho";
-import Menu from "../../componentes/Menu";
-import Banner from "../../componentes/Banner";
 import styles from './PaginaInicial.module.scss';
-import Rodape from "../../componentes/Rodape";
+
+import Banner from "componentes/Banner";
+import Cabecalho from "componentes/Cabecalho";
+import Menu from "componentes/Menu";
+import Rodape from "componentes/Rodape";
+import Galeria from "componentes/Galeria";
+import Populares from "componentes/Populares";
 
 export default function PaginaInicial() {
   return (
     <>
       <Cabecalho />
       <main>
-        <div className={styles.principal}>
+        <section className={styles.principal}>
           <Menu />
           <Banner />
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
         </div>
       </main>
       <Rodape />
